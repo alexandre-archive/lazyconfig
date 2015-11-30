60da7f000d26bd4dc5eb9b9a3db1ad87be7c2d46
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-11-30 13:49:20 -0200

    Refactor.

--- lazyconfig/__init__.py
+++ lazyconfig/__init__.py
@@ -41,50 +41,58 @@ class ConfigObject(object):
 
 
 class LazyConfig(object):
-    yaml_file = None
-    loaded = False
 
-    @classmethod
-    def set_config_file(cls, filename):
-        if cls.yaml_file != filename:
-            cls.loaded = False
-
-        cls.yaml_file = filename
+    def __init__(self, filename=None):
+        self.filename = filename
+        self.loaded = False
 
     @property
     def config(self):
-        if not LazyConfig.loaded:
+        if not self.loaded:
             self.load()
 
         return self._config
 
-    def load_dict(self, conf):
-        self._config = ConfigObject(**conf)
-        LazyConfig.loaded = True
+    @property
+    def default_config_file(self):
+        return os.path.join(os.path.dirname(sys.argv[0]), 'config.yaml')
+
+    def set_config_file(self, filename):
+        if self.filename != filename:
+            self.loaded = False
+            self.filename = filename
+
         return self
 
-    def _load_yaml_file(self, filename):
+    def load_from_dict(self, conf):
+        self._config = ConfigObject(**conf)
+        self.loaded = True
+        return self
+
+    def load_from_yaml(self, filename):
         with open(filename, 'r') as f:
-            self.load_dict(yaml.load(f))
+            self.load_from_dict(yaml.load(f))
+
+        return self
 
     def load(self, filename=None):
         if filename:
             file_to_load = filename
-        elif LazyConfig.yaml_file:
-            file_to_load = LazyConfig.yaml_file
+        elif self.filename:
+            file_to_load = self.filename
         else:
-            file_to_load = os.path.join(os.path.dirname(sys.argv[0]), 'config.yaml')
+            file_to_load = self.default_config_file
 
-        force_reload = LazyConfig.yaml_file != file_to_load
+        force_reload = self.filename != file_to_load
 
-        if force_reload or not LazyConfig.loaded:
-            LazyConfig.yaml_file = file_to_load
-            self._load_yaml_file(file_to_load)
+        if force_reload or not self.loaded:
+            self.filename = file_to_load
+            self.load_from_yaml(file_to_load)
 
         return self
 
     def reload(self):
-        LazyConfig.loaded = False
+        self.loaded = False
         return self.load()
 
 lazyconfig = LazyConfig()
--- tests/test.py
+++ tests/test.py
@@ -5,38 +5,28 @@ import unittest
 
 sys.path.append('./')
 
-from lazyconfig import lazyconfig, LazyConfig
+from lazyconfig import LazyConfig
 
 
 class TestLazyConfig(unittest.TestCase):
 
-    def setUp(self):
-        LazyConfig.yaml_file = None
-        LazyConfig.loaded = False
-
     def test_custom_filename(self):
-        self.assertEquals(LazyConfig.yaml_file, None)
-        self.assertFalse(LazyConfig.loaded)
-        lazyconfig.set_config_file('tests/data/example.yaml')
-        lazyconfig.load()
-        self.assertTrue(lazyconfig.config is not None)
-        self.assertEquals(LazyConfig.yaml_file, 'tests/data/example.yaml')
-        self.assertTrue(LazyConfig.loaded)
+        lc = LazyConfig()
+        lc.set_config_file('tests/data/example.yaml')
+        lc.load()
 
-    def test_custom_filename_class(self):
-        self.assertEquals(LazyConfig.yaml_file, None)
-        self.assertFalse(LazyConfig.loaded)
-        LazyConfig.set_config_file('tests/data/example.yaml')
-        lazyconfig.load()
-        self.assertTrue(lazyconfig.config is not None)
-        self.assertEquals(LazyConfig.yaml_file, 'tests/data/example.yaml')
-        self.assertTrue(LazyConfig.loaded)
+        self.assertTrue(lc.loaded)
+        self.assertTrue(lc.config is not None)
+        self.assertEquals(lc.filename, 'tests/data/example.yaml')
 
     def test_default_filename(self):
-        self.assertEquals(lazyconfig.config.foo.bar, 'test')
-        self.assertTrue(lazyconfig.yaml_file is not None)
-        self.assertTrue(lazyconfig.yaml_file.endswith('config.yaml'))
-        self.assertTrue(lazyconfig.loaded)
+        lc = LazyConfig()
+        lc.load()
+
+        self.assertTrue(lc.loaded)
+        self.assertTrue(lc.filename is not None)
+        self.assertTrue(lc.filename.endswith('config.yaml'))
+        self.assertEquals(lc.config.foo.bar, 'test')
 
 
 if __name__ == '__main__':
